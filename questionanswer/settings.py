68a25b68a5c5c2eaa64449e643cc23dbf6a18eda
Vikas jain <57862035+vikasjain1402@users.noreply.github.com>
2020-12-23 13:48:53 +0530

    Update settings.py
    code deleted

--- questionanswer/settings.py
+++ questionanswer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wo=nu4s3^3ys#16fyx0d(wvj4g*h#0s55+#2*c3zp9o@5g08yy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
